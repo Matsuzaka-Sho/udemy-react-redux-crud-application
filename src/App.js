dd445adc4b68602860b59f993b39e68069eb2d2a
github user name <matsuking0428@gmail.com>
2022-03-23 09:08:19 +0900

    dev:

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@ import './App.css';
 
 class App extends Component {
   render(){
-    return <h1>Hello, world!</h1>;
+    return (
+    <React.Fragment>
+      <label htmlFor='bar'>bar</label>
+      <input type='text' onChange={() => {console.log('I am clicked!')}} />;
+    </React.Fragment>
+    )
   }
 }
+
+//class App extends Component {
+//  render(){
+//    const greeting = 'Hi!'
+//    const dom = <h1 className='foo'>{greeting}</h1>
+//    return dom;
+//  }
+//}
+
+//# html部分をconstとして格納することもできる。
+//class App extends Component {
+//  render(){
+//    const dom = <h1>Hi!</h1>
+//    return dom;
+//  }
+//}
 
 //class App extends Component {
 //  render(){
