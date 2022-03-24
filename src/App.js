e7c9d77f096a3b27e85003615dc377a56270b668
github user name <matsuking0428@gmail.com>
2022-03-24 22:12:49 +0900

    DEV:learn component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render(){
-    return (
-    <React.Fragment>
-      <label htmlFor='bar'>bar</label>
-      <input type='text' onChange={() => {console.log('I am clicked!')}} />;
-    </React.Fragment>
-    )
-  }
-}
+//class App extends Component {
+//  render(){
+//    return (
+//    <React.Fragment>
+//      <label htmlFor='bar'>bar</label>
+//      <input type='text' onChange={() => {console.log('I am clicked!')}} />;
+//    </React.Fragment>
+//    )
+//  }
+//}
 
 //class App extends Component {
 //  render(){
@@ -38,4 +38,18 @@ class App extends Component {
 //  }
 //}
 
+const App = () =>{
+    return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+    )
+}
+
+const Cat = () =>{
+    return <div>Hi</div>
+}
 export default App;
